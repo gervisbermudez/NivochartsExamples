d7417f4da3a7c1750914c19c8b27c920c4625b17
Gervis Bermudez <42557396+gervisbermudez@users.noreply.github.com>
2021-06-30 14:04:26 -0300

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,272 @@
+export const works = [
+  {
+    id: "japan",
+    color: "hsl(293, 70%, 50%)",
+    data: [
+      {
+        x: "plane",
+        y: 0
+      },
+      {
+        x: "helicopter",
+        y: 232
+      },
+      {
+        x: "boat",
+        y: 242
+      },
+      {
+        x: "train",
+        y: 53
+      },
+      {
+        x: "subway",
+        y: 187
+      },
+      {
+        x: "bus",
+        y: 39
+      },
+      {
+        x: "car",
+        y: 46
+      },
+      {
+        x: "moto",
+        y: 69
+      },
+      {
+        x: "bicycle",
+        y: 273
+      },
+      {
+        x: "horse",
+        y: 215
+      },
+      {
+        x: "skateboard",
+        y: 231
+      },
+      {
+        x: "others",
+        y: 164
+      }
+    ]
+  },
+  {
+    id: "france",
+    color: "hsl(7, 70%, 50%)",
+    data: [
+      {
+        x: "plane",
+        y: 138
+      },
+      {
+        x: "helicopter",
+        y: 136
+      },
+      {
+        x: "boat",
+        y: 273
+      },
+      {
+        x: "train",
+        y: 230
+      },
+      {
+        x: "subway",
+        y: 260
+      },
+      {
+        x: "bus",
+        y: 17
+      },
+      {
+        x: "car",
+        y: 3
+      },
+      {
+        x: "moto",
+        y: 200
+      },
+      {
+        x: "bicycle",
+        y: 264
+      },
+      {
+        x: "horse",
+        y: 85
+      },
+      {
+        x: "skateboard",
+        y: 0
+      },
+      {
+        x: "others",
+        y: 58
+      }
+    ]
+  },
+  {
+    id: "us",
+    color: "hsl(332, 70%, 50%)",
+    data: [
+      {
+        x: "plane",
+        y: 109
+      },
+      {
+        x: "helicopter",
+        y: 169
+      },
+      {
+        x: "boat",
+        y: 202
+      },
+      {
+        x: "train",
+        y: 125
+      },
+      {
+        x: "subway",
+        y: 242
+      },
+      {
+        x: "bus",
+        y: 145
+      },
+      {
+        x: "car",
+        y: 288
+      },
+      {
+        x: "moto",
+        y: 202
+      },
+      {
+        x: "bicycle",
+        y: 215
+      },
+      {
+        x: "horse",
+        y: 155
+      },
+      {
+        x: "skateboard",
+        y: 109
+      },
+      {
+        x: "others",
+        y: 292
+      }
+    ]
+  },
+  {
+    id: "germany",
+    color: "hsl(72, 70%, 50%)",
+    data: [
+      {
+        x: "plane",
+        y: 250
+      },
+      {
+        x: "helicopter",
+        y: 199
+      },
+      {
+        x: "boat",
+        y: 50
+      },
+      {
+        x: "train",
+        y: 101
+      },
+      {
+        x: "subway",
+        y: 107
+      },
+      {
+        x: "bus",
+        y: 37
+      },
+      {
+        x: "car",
+        y: 190
+      },
+      {
+        x: "moto",
+        y: 221
+      },
+      {
+        x: "bicycle",
+        y: 70
+      },
+      {
+        x: "horse",
+        y: 199
+      },
+      {
+        x: "skateboard",
+        y: 130
+      },
+      {
+        x: "others",
+        y: 83
+      }
+    ]
+  },
+  {
+    id: "norway",
+    color: "hsl(259, 70%, 50%)",
+    data: [
+      {
+        x: "plane",
+        y: 187
+      },
+      {
+        x: "helicopter",
+        y: 285
+      },
+      {
+        x: "boat",
+        y: 155
+      },
+      {
+        x: "train",
+        y: 270
+      },
+      {
+        x: "subway",
+        y: 221
+      },
+      {
+        x: "bus",
+        y: 80
+      },
+      {
+        x: "car",
+        y: 291
+      },
+      {
+        x: "moto",
+        y: 181
+      },
+      {
+        x: "bicycle",
+        y: 35
+      },
+      {
+        x: "horse",
+        y: 82
+      },
+      {
+        x: "skateboard",
+        y: 10
+      },
+      {
+        x: "others",
+        y: 285
+      }
+    ]
+  }
+];
